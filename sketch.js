f87a3c85ca161488c7e4b6bbbe1079f6fd7aacaa
Gui772 <quintana.guilherme@escola.pr.gov.br>
2024-09-10 14:42:44 -0300

    adicionando js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+    background("red")
+  }
+  
+  function draw() {
+    
+    stroke("blue");
+    fill("purple");
+    
+    // console.log(mouseIsPressed);
+    
+    if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+   }
+  }
